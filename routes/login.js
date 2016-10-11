9a221110e2bf01d5ae96ee37f2bc85ff1fe03bfb
Scott Penrose <scottp@dd.com.au>
2016-10-11 17:46:58 +1100

    Fixed login and authentication

--- routes/login.js
+++ routes/login.js
@@ -12,26 +12,15 @@ module.exports = function () {
 	router.post('/', function(req, res) {
 		var login = req.param('username');
 		login = login.replace(/[^\w\s]/gi, '');
-		
+
 		if (! config.auth[login])
 			return res.json({ success: 0, error: "invalid login" });
 		if ( config.auth[login].password != req.param('password'))
 			return res.json({ success: 0, error: "invalid password" });
-		var projects = {};
-		Object.keys(config.projects).forEach(function(item) {
-			// CHECK 'auth' is real (use TRY)
-			config.projects[item].auth.forEach(function(row) {
-				if (row == login) {
-					projects[item] = {
-						title: config.projects[item].title
-					};
-				}
-			});
-		});
+
 		req.session.user = {
 			username: login,
 			email: config.auth[login].email,
-			projects: projects
 		};
 		return res.json({ success: 1, user: req.session.user});
 	});
