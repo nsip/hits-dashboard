cbed4d06c27eb386e1441c34e344a1653025e328
Scott Penrose <scottp@dd.com.au>
2017-03-08 22:59:54 +1100

    Start of report caller

--- routes/report.js
+++ routes/report.js
@@ -2,27 +2,40 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database');
 var logger = require('../logger');
+var spawn = require('child_process').spawn;
 
 // AUTHENTICATION - All entries start with base URL
 router.use(function (req, res, next) {
-	next();
+  next();
 });
 
 router.get('/:dbid/report/:reportid', function(req, res, next) {
-	var connection = db.get('hits_sif3_infra');
-	connection.query(
-		'SELECT id,requestTime,responseTime,clientIp, url, solutionId, appKey, userToken, context, instanceId, zone, environmentToken, sessionToken, method, httpStatus, requestMediaType, responseMediaType FROM XMLAudit WHERE appKey = ? ORDER BY requestTime DESC LIMIT 1000',
-    [req.params.dbid],
-		function(err, rows, fields) {
-            var ret = [];
-			if (err)
-				return res.error(err);
-			return res.json({
-				success: 1,
-				data: rows,
-			});
-		}
-	);
+  var alldata = "";
+  // XXX JSON ?
+
+  // Perl version - system("export PERL5LIB=$d/lib/; perl $d/bin/report $id $d/$report > /tmp/$$.pl 2> /tmp/$$.err");
+  // report_dir: '/Users/scottp/nsip/HITS-Reports'
+  prc = spawn('/usr/sbin/wpa_cli', ['-i','wlan0','scan_results']);
+
+  prc.on('error', function(err) {
+    logger.debug("SCAN: Error = " + err);
+  });
+
+  prc.stdout.on('data', function (data) {
+    alldata = alldata + data.toString();
+    logger.debug("SCAN: >" + data.toString());
+  });
+
+  prc.stderr.on('data', function (data) {
+    logger.debug("SCAN: !" + data.toString());
+  });
+
+  prc.on('close', function(code) {
+    return res.json({
+      data: @lines;
+    });
+  });
+
 });
 
 module.exports = router;
