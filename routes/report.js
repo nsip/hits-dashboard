fc0ccccc1ef656c71e979def72c2f40714e5390b
NSIP Update <info@nsip.edu.au>
2017-03-09 11:01:35 +0000

    Root fixed

--- routes/report.js
+++ routes/report.js
@@ -20,10 +20,11 @@ router.get('/:dbid/report/:reportid', function(req, res, next) {
   // Perl version - system("export PERL5LIB=$d/lib/; perl $d/bin/report $id $d/$report > /tmp/$$.pl 2> /tmp/$$.err");
   // report_dir: '/Users/scottp/nsip/HITS-Reports'
   // var root = os.homedir() + "/HITS-Reports";
-  var root = "/home/ubuntu" + "/HITS-Reports";
+  var home = "/home/ubuntu";
+  var root = home + "/HITS-Reports";
   var cmd = root + "/bin/report";
-  prc = spawn(root + "/bin/report", [dbid, reportid + "/in.pl"], {
-    env: {"PERL5LIB": root + "/lib"},
+  prc = spawn(root + "/bin/report", ["db/" + dbid, reportid + "/in.pl"], {
+    env: {PERL5LIB: root + "/lib", HOME: home},
     cwd: root,
   });
 
