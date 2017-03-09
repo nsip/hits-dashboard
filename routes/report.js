cdcd2197de53521a40f8c518faadd3e6ae68f048
Scott Penrose <scottp@dd.com.au>
2017-03-09 21:41:59 +1100

    Make report work ! Damn it

--- routes/report.js
+++ routes/report.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var db = require('../database');
 var logger = require('../logger');
 var spawn = require('child_process').spawn;
+const os = require('os');
 
 // AUTHENTICATION - All entries start with base URL
 router.use(function (req, res, next) {
@@ -10,29 +11,37 @@ router.use(function (req, res, next) {
 });
 
 router.get('/:dbid/report/:reportid', function(req, res, next) {
-  var alldata = "";
+  var alldata = [];
   // XXX JSON ?
+
+  var dbid = req.params.dbid;
+  var reportid = req.params.reportid;
 
   // Perl version - system("export PERL5LIB=$d/lib/; perl $d/bin/report $id $d/$report > /tmp/$$.pl 2> /tmp/$$.err");
   // report_dir: '/Users/scottp/nsip/HITS-Reports'
-  prc = spawn('/usr/sbin/wpa_cli', ['-i','wlan0','scan_results']);
+  prc = spawn(os.homedir() + '/', ['-i','wlan0','scan_results']);
+  prc = spawn(os.homedir() + '/', ['-i','wlan0','scan_results']);
+  var root = os.homedir() + "/HITS-Reports";
+  var cmd = root + "/bin/report";
+  prc = spawn(root + "/bin/report", [dbid, reportid + "/in.pl"], {env: {"PERL5LIB": root + "/lib"}});
 
   prc.on('error', function(err) {
-    logger.debug("SCAN: Error = " + err);
+    logger.debug("REPORT: Error = " + err);
   });
 
   prc.stdout.on('data', function (data) {
     alldata = alldata + data.toString();
-    logger.debug("SCAN: >" + data.toString());
+    logger.debug("REPORT: >" + data.toString());
   });
 
   prc.stderr.on('data', function (data) {
-    logger.debug("SCAN: !" + data.toString());
+    logger.debug("REPORT: !" + data.toString());
+    alldata.push(data.toString());
   });
 
   prc.on('close', function(code) {
     return res.json({
-      data: @lines;
+      data: alldata,
     });
   });
 
