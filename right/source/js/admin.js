c35f2c91f017bdd645be59158642dd9888df94bb
Scott Penrose <scottp@dd.com.au>
2017-06-27 16:45:23 +1000

    Contacts done

--- right/source/js/admin.js
+++ right/source/js/admin.js
@@ -1,47 +1,114 @@
 // ----------------------------------------------------------------------
 $(document).ready(function() {
-	var table = undefined;
-	// XXX How to get on error to redriect to api admin
-	var loadTable = function() {
-		if (!table) {
-			table = $('#admin-accounts').DataTable( {
-				"ajax": {
-					"url": "/api/admin",
-					"dataSrc": "data"
-				},
-				"order": [[ 0, "desc" ]],
-				"columns": [
-					{
-						"title": "ID",
-						"width": "25%",
-						"data": "id",
-						"render": function (data, type, row, meta) {
-							return '<a href="start.html?id=' + data + '">' + data + '</a>';
-						},
-						"defaultContent": '',
+
+	if ($('#admin-accounts').length) {
+
+		var table = undefined;
+		// XXX How to get on error to redriect to api admin
+		var loadTable = function() {
+			if (!table) {
+				table = $('#admin-accounts').DataTable( {
+					"ajax": {
+						"url": "/api/admin",
+						"dataSrc": "data"
 					},
-					{ "title": "Name", "width": "20%", "data": "name", "defaultContent": '', },
-					{ "title": "Email", "width": "20%", "data": "email", "defaultContent": '', },
-					{ "title": "Databases", "width": "10%", "data": "count", defaultContent: "None", "defaultContent": '', },
-					{ "title": "Recent", "width": "20%", "data": "recent", defaultContent: "None", "defaultContent": '', }
-					// Adding buttons
-				]
-			} );
+					"order": [[ 0, "desc" ]],
+					"columns": [
+						{
+							"title": "ID",
+							"width": "25%",
+							"data": "id",
+							"render": function (data, type, row, meta) {
+								return '<a href="start.html?id=' + data + '">' + data + '</a>';
+							},
+							"defaultContent": '',
+						},
+						{ "title": "Name", "width": "20%", "data": "name", "defaultContent": '', },
+						{ "title": "Email", "width": "20%", "data": "email", "defaultContent": '', },
+						{ "title": "Databases", "width": "10%", "data": "count", defaultContent: "None", "defaultContent": '', },
+						{ "title": "Recent", "width": "20%", "data": "recent", defaultContent: "None", "defaultContent": '', }
+						// Adding buttons
+					]
+				} );
 
-			/*
-			$('#filedata tbody').on('click', 'tr', function () {
-				var data = table.row( this ).data();
-				window.location = "/ui/processed.html?dir=" + data.id;
-			});
-			*/
-		}
-		else {
-			table.ajax.reload();
-		}
-	};
+				/*
+				$('#filedata tbody').on('click', 'tr', function () {
+					var data = table.row( this ).data();
+					window.location = "/ui/processed.html?dir=" + data.id;
+				});
+				*/
+			}
+			else {
+				table.ajax.reload();
+			}
+		};
 
-	loadTable();
-	setInterval(loadTable, 10000);
+		loadTable();
+		setInterval(loadTable, 10000);
+	}
+
+	if ($('#admin-contacts').length) {
+
+		var table = undefined;
+		// XXX How to get on error to redriect to api admin
+		var loadTable = function() {
+			if (!table) {
+				table = $('#admin-contacts').DataTable( {
+					"ajax": {
+						"url": "/api/admin/contact",	// XXX Overloaded use of URL
+						"dataSrc": "data"
+					},
+					"order": [[ 0, "desc" ]],
+					"columns": [
+						{
+							"title": "ID",
+							"width": "25%",
+							"data": "id",
+							"render": function (data, type, row, meta) {
+								// XXX Mark as done etc.
+								//	TODO - Copy fields from this into Account Create - allow creation. Automatic ?
+								//	TODO - Mark this as wip, or done where required
+								return data; // '<a href="start.html?id=' + data + '">' + data + '</a>';
+							},
+							"defaultContent": '',
+						},
+						{ "title": "Created", "width": "20%", "data": "created_at", "defaultContent": '', },
+						{ "title": "Status", "width": "20%", "data": "status", "defaultContent": '', },
+						{ "title": "Data", "width": "50%", "data": "data", defaultContent: "None", "defaultContent": '',
+							"render": function (data, type, row, meta) {
+								var rows = [];
+								var raw;
+								try {
+									var raw = JSON.parse(data);
+								}
+								catch(e) {
+									return data;
+								}
+								$.each(raw, function(key) {
+									rows.push(key + " = " + raw[key]);
+								});
+								return rows.join("<br />\n");
+							},
+						},
+						// Adding buttons
+					]
+				} );
+
+				/*
+				$('#filedata tbody').on('click', 'tr', function () {
+					var data = table.row( this ).data();
+					window.location = "/ui/processed.html?dir=" + data.id;
+				});
+				*/
+			}
+			else {
+				table.ajax.reload();
+			}
+		};
+
+		loadTable();
+		setInterval(loadTable, 60000);
+	}
 
 	// Create new entry
 	$('#create-create').click(function() {
@@ -70,6 +137,5 @@ $(document).ready(function() {
 
 		return false;
 	});
-
 
 });
