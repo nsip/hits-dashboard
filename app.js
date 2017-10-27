5f643a108bb6657e82da7137bfc1a29aa6b5125f
Scott Penrose <scottp@dd.com.au>
2017-10-27 17:48:15 +1100

    Remove public symlink

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'right/dist')));
 app.use("/usecases", express.static(path.join(__dirname, '../usecases/raw')));
 
 // TODO Separate entry, location, files etc
