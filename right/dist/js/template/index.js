3a5f5c566ac30e870044ddc36d79bcd89b29fa87
Frances Gray <nashiralyana@gmail.com>
2018-01-31 17:57:50 +1100

    Adds the ability to do email recovery to the hits dashboard #210

--- right/dist/js/template/index.js
+++ right/dist/js/template/index.js
@@ -51,4 +51,28 @@ $(document).ready(function() {
 
 		$('.selectpicker').selectpicker();
 	}
+
+	if($('.recoverEmailSubmit').length){
+	    $('.recoverEmailSubmit').on("click", function() {
+	        
+	        var email = $('.recoverEmailAddress').val();
+	        
+	        $.ajax({
+	            url: "/api/recover/?email=" + email,
+	            type: 'GET',
+	            success: function(result) {
+	                try {
+
+	                    // reset the recovery email address
+	                    $('.recoverEmailAddress').val('');
+	                    $('.emailSentSuccessMessage').show();
+
+	                } catch(e) {
+	                    alert("There was a problem sending a recovery email: " + e);
+	                }
+	            }
+	        });    
+	    }); 
+	}
+
 });
