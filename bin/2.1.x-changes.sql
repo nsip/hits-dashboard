d92a0f7af4db61e199562670ca336888022822ca
Scott Penrose <scottp@dd.com.au>
2018-06-05 13:26:39 +1000

    New DB

---
+++ bin/2.1.x-changes.sql
@@ -0,0 +1,327 @@
+alter table Invoice add FormNumber VARCHAR(200);
+alter table FinancialAccount add ClassType VARCHAR(200);
+alter table TimeTableSubject add academicYearStart VARCHAR(200), add academicYearEnd varchar(200);
+alter table TimeTableCell add SchoolInfo_LocalId VARCHAR(200);
+
+create table PersonalisedPlan (
+RefId varchar(36) NOT NULL,
+StudentPersonal_RefId varchar(36) DEFAULT NULL,
+SchoolInfo_RefId varchar(36) DEFAULT NULL,
+PersonalisedPlanCategory varchar(200) DEFAULT NULL,
+PersonalisedPlanStartDate varchar(200) DEFAULT NULL,
+PersonalisedPlanEndDate varchar(200) DEFAULT NULL,
+PersonalisedPlanReviewDate varchar(200) DEFAULT NULL,
+PersonalisedPlanNotes varchar(1000) DEFAULT NULL,
+AssociatedAttachment varchar(200) DEFAULT NULL,
+PRIMARY KEY (RefId)
+);
+
+create table PersonalisedPlan_Document (
+PersonalisedPlan_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+Location varchar(200) DEFAULT NULL,
+Sensitivity varchar(200) DEFAULT NULL,
+URL varchar(200) DEFAULT NULL,
+DocumentType varchar(200) DEFAULT NULL,
+DocumentReviewDate varchar(200) DEFAULT NULL,
+DocumentDescription varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY PersonalisedPlan_Document_IX (PersonalisedPlan_RefId)
+);
+
+create table WellbeingResponse (
+RefId varchar(36) NOT NULL,
+StudentPersonal_RefId varchar(36) DEFAULT NULL,
+SchoolInfo_RefId varchar(36) DEFAULT NULL,
+Date varchar(200) DEFAULT NULL,
+WellbeingResponseStartDate varchar(200) DEFAULT NULL,
+WellbeingResponseEndDate varchar(200) DEFAULT NULL,
+WellbeingResponseCategory varchar(200) DEFAULT NULL,
+WellbeingResponseNotes varchar(1000) DEFAULT NULL,
+SuspensionContainer_SuspensionCategory varchar(200) DEFAULT NULL,
+SuspensionContainer_Duration varchar(200) DEFAULT NULL,
+SuspensionContainer_AdvisementDate varchar(200) DEFAULT NULL,
+SuspensionContainer_ResolutionMeetingTime varchar(200) DEFAULT NULL,
+SuspensionContainer_ResolutionNotes varchar(1000) DEFAULT NULL,
+SuspensionContainer_EarlyReturnDate varchar(200) DEFAULT NULL,
+SuspensionContainer_Status varchar(200) DEFAULT NULL,
+DetentionContainer_DetentionCategory varchar(200) DEFAULT NULL,
+DetentionContainer_DetentionDate varchar(200) DEFAULT NULL,
+DetentionContainer_DetentionLocation varchar(200) DEFAULT NULL,
+DetentionContainer_DetentionNotes varchar(1000) DEFAULT NULL,
+DetentionContainer_Status varchar(200) DEFAULT NULL,
+PlanRequiredContainer_Status varchar(200) DEFAULT NULL,
+AwardContainer_AwardDate  varchar(200) DEFAULT NULL,
+AwardContainer_AwardType  varchar(200) DEFAULT NULL,
+AwardContainer_AwardDescription  varchar(200) DEFAULT NULL,
+AwardContainer_AwardNotes  varchar(1000) DEFAULT NULL,
+AwardContainer_Status varchar(200) DEFAULT NULL,
+OtherWellbeingResponseContainer_OtherResponseDate  varchar(200) DEFAULT NULL,
+OtherWellbeingResponseContainer_OtherResponseType  varchar(200) DEFAULT NULL,
+OtherWellbeingResponseContainer_OtherResponseDescription  varchar(200) DEFAULT NULL,
+OtherWellbeingResponseContainer_OtherResponseNotes  varchar(1000) DEFAULT NULL,
+OtherWellbeingResponseContainer_Status  varchar(200) DEFAULT NULL,
+PRIMARY KEY (RefId)
+);
+
+create table WellbeingResponse_SuspensionContainer_WithdrawalTime (
+WellbeingResponse_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+WithdrawalDate  varchar(200) DEFAULT NULL,
+WithdrawalStartTime  varchar(200) DEFAULT NULL,
+WithdrawalEndTime  varchar(200) DEFAULT NULL,
+TimeTableSubject_RefId  varchar(36) DEFAULT NULL,
+ScheduledActivity_RefId  varchar(36) DEFAULT NULL,
+TimeTableCell_RefId  varchar(36) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingResponse_Document_IX (WellbeingResponse_RefId)
+);
+
+create table WellbeingResponse_PlanRequiredContainer_PlanRequired (
+WellbeingResponse_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+PersonalisedPlan_RefId  varchar(36) DEFAULT NULL,
+PlanNotes  varchar(1000) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingResponse_Document_IX (WellbeingResponse_RefId)
+);
+
+create table WellbeingResponse_PersonInvolvement (
+WellbeingResponse_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+PersonRefId varchar(36) DEFAULT NULL,
+PersonRefId_SIF_RefObject varchar(200) DEFAULT NULL,
+ShortName varchar(200) DEFAULT NULL,
+HowInvolved varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingResponse_Document_IX (WellbeingResponse_RefId)
+);
+
+create table WellbeingResponse_Document (
+WellbeingResponse_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+Location varchar(200) DEFAULT NULL,
+Sensitivity varchar(200) DEFAULT NULL,
+URL varchar(200) DEFAULT NULL,
+DocumentType varchar(200) DEFAULT NULL,
+DocumentReviewDate varchar(200) DEFAULT NULL,
+DocumentDescription varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingResponse_Document_IX (WellbeingResponse_RefId)
+);
+
+
+
+
+create table WellbeingEvent (
+RefId varchar(36) NOT NULL,
+StudentPersonal_RefId varchar(36) DEFAULT NULL,
+SchoolInfo_RefId varchar(36) DEFAULT NULL,
+EventId  varchar(200) DEFAULT NULL,
+WellbeingEventNotes  varchar(1000) DEFAULT NULL,
+WellbeingEventCategoryClass  varchar(200) DEFAULT NULL,
+ReportingStaffRefId  varchar(36) DEFAULT NULL,
+WellbeingEventCreationTimeStamp  varchar(200) DEFAULT NULL,
+WellbeingEventDate  varchar(200) DEFAULT NULL,
+WellbeingEventTime  varchar(200) DEFAULT NULL,
+WellbeingEventDescription  varchar(200) DEFAULT NULL,
+WellbeingEventTimePeriod  varchar(200) DEFAULT NULL,
+WellbeingEventLocationDetails_EventLocation varchar(200) DEFAULT NULL,
+WellbeingEventLocationDetails_Class varchar(200) DEFAULT NULL,
+WellbeingEventLocationDetails_FurtherLocationNotes varchar(1000) DEFAULT NULL,
+ConfidentialFlag  varchar(200) DEFAULT NULL,
+Status  varchar(200) DEFAULT NULL,
+PRIMARY KEY (RefId)
+);
+
+create table WellbeingEvent_Document (
+WellbeingEvent_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+Location varchar(200) DEFAULT NULL,
+Sensitivity varchar(200) DEFAULT NULL,
+URL varchar(200) DEFAULT NULL,
+DocumentType varchar(200) DEFAULT NULL,
+DocumentReviewDate varchar(200) DEFAULT NULL,
+DocumentDescription varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingEvent_Document_IX (WellbeingEvent_RefId)
+);
+
+create table WellbeingEvent_Category (
+WellbeingEvent_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+EventCategory varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingEvent_Category_IX (WellbeingEvent_RefId)
+);
+
+create table WellbeingEvent_Subcategory (
+WellbeingEvent_RefId varchar(36) NOT NULL,
+WellbeingEvent_CategoryId int(11) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+EventCategory varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingEvent_Subcategory_IX (WellbeingEvent_RefId),
+KEY WellbeingEvent_Subcategory_Category_IX (WellbeingEvent_CategoryId),
+CONSTRAINT WellbeingEvent_Subcategory_Category_FK FOREIGN KEY (WellbeingEvent_CategoryId) REFERENCES WellbeingEvent_Category (id)
+);
+
+create table WellbeingEvent_PersonInvolvement (
+WellbeingEvent_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+PersonRefId varchar(36) DEFAULT NULL,
+PersonRefId_SIF_RefObject varchar(200) DEFAULT NULL,
+ShortName varchar(200) DEFAULT NULL,
+HowInvolved varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingEvent_PersonInvolvement_IX (WellbeingEvent_RefId)
+);
+
+create table WellbeingEvent_FollowupAction (
+WellbeingEvent_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+WellbeingResponse_RefId varchar(36) DEFAULT NULL,
+FollowUpDetails varchar(1000) DEFAULT NULL,
+FollowUpActionCategory varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingEvent_FollowupAction_IX (WellbeingEvent_RefId)
+);
+
+create table WellbeingCharacteristic (
+RefId varchar(36) NOT NULL,
+StudentPersonal_RefId varchar(36) DEFAULT NULL,
+SchoolInfo_RefId varchar(36) DEFAULT NULL,
+WellbeingCharacteristicClassification varchar(200) DEFAULT NULL,
+WellbeingCharacteristicStartDate varchar(200) DEFAULT NULL,
+WellbeingCharacteristicEndDate varchar(200) DEFAULT NULL,
+WellbeingCharacteristicReviewDate varchar(200) DEFAULT NULL,
+WellbeingCharacteristicNotes varchar(1000) DEFAULT NULL,
+WellbeingCharacteristicCategory varchar(200) DEFAULT NULL,
+WellbeingCharacteristicSubcategory varchar(200) DEFAULT NULL,
+WellbeingCharacteristicLocalCharacteristicCode varchar(200) DEFAULT NULL,
+WellbeingCharacteristicCharacteristicSeverity varchar(200) DEFAULT NULL,
+WellbeingCharacteristicDailyManagement varchar(200) DEFAULT NULL,
+WellbeingCharacteristicEmergencyManagement varchar(200) DEFAULT NULL,
+WellbeingCharacteristicEmergencyResponsePlan varchar(200) DEFAULT NULL,
+WellbeingCharacteristicTrigger varchar(200) DEFAULT NULL,
+WellbeingCharacteristicConfidentialFlag varchar(200) DEFAULT NULL,
+WellbeingCharacteristicAlert varchar(200) DEFAULT NULL,
+PRIMARY KEY (RefId)
+);
+
+create table WellbeingCharacteristic_Document (
+WellbeingCharacteristic_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+Location varchar(200) DEFAULT NULL,
+Sensitivity varchar(200) DEFAULT NULL,
+URL varchar(200) DEFAULT NULL,
+DocumentType varchar(200) DEFAULT NULL,
+DocumentReviewDate varchar(200) DEFAULT NULL,
+DocumentDescription varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingCharacteristic_Document_IX (WellbeingCharacteristic_RefId)
+);
+
+create table WellbeingCharacteristic_Medication (
+WellbeingCharacteristic_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+MedicationName varchar(200) DEFAULT NULL,
+Dosage varchar(200) DEFAULT NULL,
+Frequency varchar(200) DEFAULT NULL,
+AdministrationInformation varchar(200) DEFAULT NULL,
+Method varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingCharacteristic_Medication_IX (WellbeingCharacteristic_RefId)
+);
+
+create table WellbeingAppeal (
+RefId varchar(36) NOT NULL,
+StudentPersonal_RefId varchar(36) DEFAULT NULL,
+SchoolInfo_RefId varchar(36) DEFAULT NULL,
+WellbeingResponse_RefId varchar(36) DEFAULT NULL,
+LocalAppealId  varchar(200) DEFAULT NULL,
+AppealStatusCode  varchar(200) DEFAULT NULL,
+Date  varchar(200) DEFAULT NULL,
+AppealNotes  varchar(1000) DEFAULT NULL,
+AppealOutcome  varchar(200) DEFAULT NULL,
+PRIMARY KEY (RefId)
+);
+
+create table WellbeingAppeal_Document (
+WellbeingAppeal_RefId varchar(36) NOT NULL,
+Id int(11) NOT NULL AUTO_INCREMENT,
+Location varchar(200) DEFAULT NULL,
+Sensitivity varchar(200) DEFAULT NULL,
+URL varchar(200) DEFAULT NULL,
+DocumentType varchar(200) DEFAULT NULL,
+DocumentReviewDate varchar(200) DEFAULT NULL,
+DocumentDescription varchar(200) DEFAULT NULL,
+PRIMARY KEY (id),
+KEY WellbeingAppeal_Document_IX (WellbeingAppeal_RefId)
+);
+
+create table WellbeingAlert (
+RefId varchar(36) NOT NULL,
+StudentPersonal_RefId varchar(36) DEFAULT NULL,
+SchoolInfo_RefId varchar(36) DEFAULT NULL,
+Date  varchar(200) DEFAULT NULL,
+WellbeingAlertStartDate varchar(200) DEFAULT NULL,
+WellbeingAlertEndDate varchar(200) DEFAULT NULL,
+WellbeingAlertCategory varchar(200) DEFAULT NULL,
+WellbeingAlertDescription varchar(200) DEFAULT NULL,
+EnrolmentRestricted varchar(200) DEFAULT NULL,
+AlertAudience varchar(200) DEFAULT NULL,
+AlertSeverity varchar(200) DEFAULT NULL,
+AlertKeyContact varchar(200) DEFAULT NULL,
+PRIMARY KEY (RefId)
+);
+
+create table StudentAttendanceTimeList_PeriodAttendance (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    studentAttendanceTimeList_RefId varchar(36) NOT NULL,
+    attendanceType varchar(200) DEFAULT NULL,
+    attendanceCode varchar(200) DEFAULT NULL,
+    attendanceStatus varchar(200) DEFAULT NULL,
+    attendanceDate varchar(200) DEFAULT NULL,
+    sessionInfo_RefId varchar(36) DEFAULT NULL,
+    scheduledActivity_RefId varchar(36) DEFAULT NULL,
+    timetablePeriod varchar(200) DEFAULT NULL,
+    dayId varchar(200) DEFAULT NULL,
+    startTime varchar(200) DEFAULT NULL,
+    endTime varchar(200) DEFAULT NULL,
+    timeIn varchar(200) DEFAULT NULL,
+    timeOut varchar(200) DEFAULT NULL,
+    timeTableCell_RefId varchar(36) DEFAULT NULL,
+    timeTableSubject_RefId varchar(36) DEFAULT NULL,
+    attendanceNote varchar(200) DEFAULT NULL);
+
+ALTER TABLE StudentAttendanceTimeList_PeriodAttendance
+ADD INDEX StudentAttendanceTimeList_PeriodAttendance_IX (studentAttendanceTimeList_RefId);
+
+create table StudentAttendanceTimeList_PeriodAttendance_RoomInfo (
+    PeriodAttendance_Id INT NOT NULL,
+    RoomInfo_RefId varchar(36) NOT NULL,
+    primary key (PeriodAttendance_Id, RoomInfo_RefId),
+    index roominfo_periodattendance_ix (PeriodAttendance_Id),
+    constraint roominfo_periodattendance_fk foreign key (PeriodAttendance_Id) references StudentAttendanceTimeList_PeriodAttendance (id));
+    
+create table StudentAttendanceTimeList_PeriodAttendance_TeacherCover (
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   PeriodAttendance_Id INT NOT NULL,
+	staffPersonal_RefId varchar(36) DEFAULT NULL,
+    staffLocalId varchar(200) DEFAULT NULL,
+    startTime varchar(200) DEFAULT NULL,
+    finishTime varchar(200) DEFAULT NULL,
+    credit varchar(200) DEFAULT NULL,
+    supervision varchar(200) DEFAULT NULL,
+    weighting varchar(200) DEFAULT NULL,
+    index teachercover_periodattendance_ix (PeriodAttendance_Id),
+    constraint teachercover_periodattendance_fk foreign key (PeriodAttendance_Id) references StudentAttendanceTimeList_PeriodAttendance (id));
+
+create table StudentAttendanceTimeList_PeriodAttendance_OtherCode (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    PeriodAttendance_Id INT NOT NULL,
+    OtherCode varchar(200) DEFAULT NULL,
+    CodeSet varchar(200) DEFAULT NULL,
+    index OtherCode_periodattendance_ix (PeriodAttendance_Id),
+    constraint OtherCode_periodattendance_fk foreign key (PeriodAttendance_Id) references StudentAttendanceTimeList_PeriodAttendance (id));
--- bin/update_sql.js
+++ bin/update_sql.js
@@ -1,76 +1,77 @@
 // var uuid = require('node-uuid');
 var db = require('../database');
 var async = require('async');
+var fs = require('fs')
 
-var updateSQL = [
-  " \
-            -- #170 \
-            ALTER TABLE TimeTable_Day \
-            ADD COLUMN id INT(11) NOT NULL AUTO_INCREMENT FIRST, \
-            ADD PRIMARY KEY (id); \
-  ",
-  " \
-            ALTER TABLE TimeTable_Period \
-            ADD COLUMN id INT(11) NOT NULL AUTO_INCREMENT FIRST, \
-            ADD COLUMN TimeTable_Day_Id INT(11), \
-            ADD PRIMARY KEY (id); \
-  ",
-  " \
-              UPDATE TimeTable_Period p set TimeTable_Day_Id = (select min(d.id) from TimeTable_Day d where d.TimeTable_RefId = p.TimeTable_RefId and d.DayId = p.DayId); \
-  ",
-  " \
-            ALTER TABLE TimeTable_Period \
-            DROP FOREIGN KEY TimeTable_Period_ibfk_1; \
-  ",
-  " \
-            ALTER TABLE TimeTable_Period \
-            CHANGE COLUMN TimeTable_Day_Id TimeTable_Day_Id INT(11) NOT NULL , \
-            ADD INDEX TimeTable_Period_ibfk_1_idx (TimeTable_Day_Id ASC); \
-  ",
-  " \
-            ALTER TABLE TimeTable_Period \
-            ADD CONSTRAINT TimeTable_Period_ibfk_1 \
-            FOREIGN KEY (TimeTable_Day_Id) \
-            REFERENCES TimeTable_Day (id); \
-  ",
-  " \
-            -- #154 \
-            ALTER TABLE TeachingGroup ADD COLUMN TimeTableSubject_RefId VARCHAR(36) NULL; \
-  ",
-];
+var updateSQL;
+async.series(
+  [
 
+    // Load SQL Files
+    function(nexttop) {
+      filename = process.argv[2];
+      fs.readFile(filename, 'ascii', function(err, data) {
+        if (err) {
+          nexttop(err);
+          return;
+        }
+        console.log('OK: ' + filename);
+        data = data.replace(/\n;/gm,";");
 
-var infraconnection = db.infra();
-infraconnection.query(
-  'SELECT databaseUrl FROM APPKEY_DB_URL_MAPPER',
-  // [ req.params.dbid ],
-  function(err, dbrows, fields) {
-    console.log(err, dbrows);
+        updateSQL = data.split(/;/);
+        updateSQL = updateSQL.filter(function(n){ return ((n != "") && (n != "\n")) });
 
-    async.mapSeries(
-      dbrows,
-      function(row, next) {
-        var inst = db.get(row.databaseUrl);
+        console.log("Found " + updateSQL.length + " rules to apply");
+        // console.log(updateSQL);
 
-        async.mapSeries(
-          updateSQL,
-          function(sql, nextsql) {
-            inst.query(
-              sql,
-              function(lerr, ldbrows, lfields) {
-                console.log(row, lerr, ldbrows);
-                nextsql();
-              }
-            );
-          },
-          function() {
-            next();
-          }
-        );
-      },
-      function(err) {
-        console.log("FINSIHED");
-      }
-    );
+        nexttop();
+      });
+    },
+
+    // Execute the SQL
+    function(nexttop) {
+      var infraconnection = db.infra();
+      infraconnection.query(
+        'SELECT databaseUrl FROM APPKEY_DB_URL_MAPPER',
+        // [ req.params.dbid ],
+        function(err, dbrows, fields) {
+          console.log(err, dbrows);
+
+          async.mapSeries(
+            dbrows,
+            function(row, next) {
+              var inst = db.get(row.databaseUrl);
+
+              async.mapSeries(
+                updateSQL,
+                function(sql, nextsql) {
+                  inst.query(
+                    sql,
+                    function(lerr, ldbrows, lfields) {
+                      console.log(row, lerr, ldbrows);
+                      nextsql();
+                    }
+                  );
+                },
+                function() {
+                  next();
+                }
+              );
+            },
+            function(err) {
+              console.log("FINSIHED");
+              nexttop();
+            }
+          );
+        }
+      );
+
+    },
+  ],
+  function(err) {
+    if (err) {
+      console.log("ERROR ENDING: " + err, err);
+    }
+    console.log("End");
   }
 );
