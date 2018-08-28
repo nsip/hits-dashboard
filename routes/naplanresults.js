02a65e19620470d0ae0f1a44ca6a0d90acf4dc9d
Scott Penrose <scottp@dd.com.au>
2018-08-28 14:56:37 +1000

    Generated token

--- routes/naplanresults.js
+++ routes/naplanresults.js
@@ -5,37 +5,91 @@ var config = require('config');
 var crypto = require('crypto');
 var moment = require('moment');
 
-var checkAuth = function() {
-	var method = 'xyz';
-	var userPart = 'ABC';
-	const secret = 'abcdefg';
-
-	const hash = crypto
-		.createHmac('sha256', secret)
-		.update('I love cupcakes')
-		.digest('base64');
-	console.log(hash);
-}
+var secret = "XYZ";
 
 // Authentication !
 router.use(function (req, res, next) {
 	// TODO
 	console.log('XXX Naplan Results Auth');
 
-	if (
-		req
-		&& req.headers
-		&& req.headers.authorization
-	) {
-		checkAuth();
-		console.log("Got a header", req.headers.authorization);
-	}
-	else {
+	if (!req.headers.authorization) {
 		return res.status(401).json({
 			success: false,
 			error: 'Must supply an Authorization header',
 		});
 	}
+
+	var timestamp = req.headers.timestamp;
+	if (!timestamp) {
+		return res.status(401).json({
+			success: false,
+			error: 'Must supply a timestamp header',
+		});
+	}
+
+	/*
+	if (!req.req.headers["x-signature"]) {
+		return res.status(401).json({
+			success: false,
+			error: 'Must supply an x-signature header',
+		});
+	}
+	*/
+
+
+	// XXX Check timestamp is within a valid range
+
+	var auth = req.headers.authorization;
+	var auth_bits = auth.split(" ");
+
+	if (auth_bits.length != 2) {
+		return res.status(401).json({
+			success: false,
+			error: 'Must supply auth type and token',
+		});
+	}
+
+	if (auth_bits[0] != 'SIF_HMACSHA256') {
+		return res.status(401).json({
+			success: false,
+			error: 'Auth type must be SIF_HMACSHA256',
+		});
+	}
+
+	var token_bits = auth_bits[1].split(":");
+
+	if (token_bits.length != 2) {
+		return res.status(401).json({
+			success: false,
+			error: 'Must supply auth with a user and token',
+		});
+	}
+
+	const hash = crypto
+		.createHmac('sha256', secret)
+		// Add the USER (trusted for now) & Timestamp
+		.update(token_bits[0] + ":" + timestamp)
+		.digest('base64');
+	console.log(hash);
+
+	if (token_bits[1] != hash) {
+		return res.status(401).json({
+			success: false,
+			error: 'Token does not match provided',
+			debug: {
+				provided_user: token_bits[0],
+				provided_token: token_bits[1],
+				generated_token: hash,
+			},
+		});
+	}
+
+	/*
+	return res.status(401).json({
+		success: false,
+		error: 'Must supply an Authorization header',
+	});
+	*/
 	next();
 });
 
