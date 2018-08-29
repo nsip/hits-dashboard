e24f4fcf5feea6565633eab873cd9ac31f4608f0
Scott Penrose <scottp@dd.com.au>
2018-08-29 17:45:18 +1000

    Better URL

--- test/naplan_results.js
+++ test/naplan_results.js
@@ -15,8 +15,8 @@ var full = (new Buffer.from(user + ":" + hash)).toString('base64');
 // Send request
 request.get(
    {
-     // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
-     url: "http://localhost:3000/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
+     url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
+     // url: "http://localhost:3000/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
      headers: {
        'Authorization': 'SIF_HMACSHA256 ' + full,
        'timestamp': timestamp,
