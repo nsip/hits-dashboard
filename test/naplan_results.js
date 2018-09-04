92e2c43f13f37f9ef71e3da0ec8e69f065edeaf1
Scott Penrose <scottp@dd.com.au>
2018-09-04 16:21:44 +1000

    GZIP Test

--- test/naplan_results.js
+++ test/naplan_results.js
@@ -17,18 +17,19 @@ var full = (new Buffer.from(user + ":" + hash)).toString('base64');
 // Send request
 request.get(
    {
-     // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
+     url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
      // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolList",
-     url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/TestData",
+     // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/TestData",
      // url: "http://localhost:3000/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
      headers: {
        'Authorization': 'SIF_HMACSHA256 ' + full,
        'timestamp': timestamp,
      },
      json: {},
-     gzip: true,
+      gzip: true,
    },
    function (error, response, body) {
+     // console.log(response);
      if (!error) {
        console.log("Body", body);
      }
