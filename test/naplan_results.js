64a78294ca5f0ae0eb5946fead2e21a15741c228
Scott Penrose <scottp@dd.com.au>
2018-09-05 20:15:06 +1000

    Localhsot

--- test/naplan_results.js
+++ test/naplan_results.js
@@ -3,7 +3,7 @@ var moment = require('moment');
 var request = require('request');
 
 var secret = "nnn6WWgqVoHh";
-var user = "vicgovXXX";
+var user = "vicgov";
 
 // Push timestamp forward/back for testing (add or subtract)
 var timestamp = moment().add('0', 'minutes').toISOString();
@@ -17,10 +17,10 @@ var full = (new Buffer.from(user + ":" + hash)).toString('base64');
 // Send request
 request.get(
    {
-     // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
+     url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
      // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolList",
      // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/TestData",
-     url: "http://localhost:3000/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
+     // url: "http://localhost:3000/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
      headers: {
        'Authorization': 'SIF_HMACSHA256 ' + full,
        'timestamp': timestamp,
