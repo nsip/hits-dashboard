d001e5a69bf88da172d602db08bd32f676e8e123
Scott Penrose <scottp@dd.com.au>
2018-09-18 06:48:02 +0000

    Automatic build output files

--- right/source/js/usecase.js
+++ right/source/js/usecase.js
@@ -6,5 +6,7 @@ $(document).ready(function() {
         frame.style.height = frame.contentWindow.document.body.scrollHeight + 'px';
         
         $('body.main-scrollable .main__scroll').scrollTop(0);
+
+	// $('#link_other a').attr('target', '_blank');
     }); 
 });
