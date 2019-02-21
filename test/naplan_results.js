f5e4f70c2a3582c971cede624a67c7c5fb90e91a
Scott Penrose <scottp@dd.com.au>
2019-02-22 10:21:10 +1100

    Update test code

--- test/naplan_results.js
+++ test/naplan_results.js
@@ -20,10 +20,16 @@ request.get(
      // url: "http://hits.nsip.edu.au/api/naplanresults/SchoolData/3afabf3d-cebe-42ce-97d4-0c1444628407",
      // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
      // url: "http://hits.nsip.edu.au/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
-     url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolList",
+     // url: "http://hitstest.dev.nsip.edu.au/api/naplanresults/SchoolList",
      // url: "http://hits.nsip.edu.au/api/naplanresults/TestData",
+
+     // Feb 2019
      // url: "http://hits.nsip.edu.au/api/naplanresults/SchoolList",
-     // url: "http://localhost:3000/api/naplanresults/SchoolData/0bb5dfc0-0aa1-46ca-9533-a7af9eb02b65",
+     // url: "http://hits.nsip.edu.au/api/naplanresults/SchoolData/f015c5ef-5b32-4558-b289-c4416d52f770",
+     // url: "http://hits.nsip.edu.au/api/naplantest/large/testdata",
+     // url: "http://hits.nsip.edu.au/api/naplantest/large/schoollist",
+     // url: "http://hits.nsip.edu.au/api/naplantest/large/schooldata/7100bc33-8f11-41af-8230-ce52245621fb",
+     url: "http://hits.nsip.edu.au/api/naplantest/large/SchoolData/7100bc33-8f11-41af-8230-ce52245621fb",
      headers: {
        'Authorization': 'SIF_HMACSHA256 ' + full,
        'timestamp': timestamp,
---
+++ test/naplan_results_loop.js
@@ -0,0 +1,89 @@
+var async = require('async');
+var crypto = require('crypto');
+var moment = require('moment');
+var request = require('request');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+var findDeep = require('find-deep');
+
+var secret = "nnn6WWgqVoHh";
+var user = "vicgov";
+
+// Push timestamp forward/back for testing (add or subtract)
+var timestamp = moment().add('0', 'minutes').toISOString();
+
+const hash = crypto
+  .createHmac('sha256', secret)
+  .update(user + ":" + timestamp)
+  .digest('base64');
+var full = (new Buffer.from(user + ":" + hash)).toString('base64');
+
+// Get list of School IDs
+request.get(
+   {
+     url: "http://hits.nsip.edu.au:3000/api/naplantest/large/SchoolList/",
+     headers: {
+       'Authorization': 'SIF_HMACSHA256 ' + full,
+       'timestamp': timestamp,
+     },
+     json: {},
+      gzip: true,
+   },
+   function (error, response, body) {
+     // console.log(response);
+     if (!error) {
+       console.log("Status", response.statusCode);
+       // console.log("Body", body);
+
+        parser.parseString(body, function(err,result){
+          console.log("Parse Err", err);
+          // console.log("Parse Results", result);
+          // extractedData = result['data'];
+          var schoolinfo = findDeep(result, (value, key) =>
+            key == 'SchoolInfo'
+          );
+
+          var count = 0;
+          async.eachLimit(
+            schoolinfo,
+            // NOTE: Concurrency - How many to perform at once
+            100,
+            function(school, next) {
+              count++;
+              var RefId = school['$'].RefId;
+              console.log(RefId + " START " + count + " of " + schoolinfo.length);
+              request.get(
+                 {
+                   url: "http://hits.nsip.edu.au:3000/api/naplantest/large/SchoolData/" + RefId,
+                   headers: {
+                     'Authorization': 'SIF_HMACSHA256 ' + full,
+                     'timestamp': timestamp,
+                   },
+                   json: {},
+                    gzip: true,
+                 },
+                 function (error, response, body) {
+                   if (error) {
+                    console.log(RefId + " ERROR = " + error);
+                  }
+                  else {
+                    // XXX Check data returned is valid
+                    console.log(RefId + " SUCCESS + " + response.statusCode);
+                  }
+                  next();
+                }
+              );
+            },
+            function(err) {
+              if (err)
+                console.log("ERROR", err);
+              console.log("FINISHED");
+            }
+          );
+        });
+     }
+     else {
+       console.log("Error", error);
+     }
+   }
+);
